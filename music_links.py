bc1667b7a0437f69b47b5b8ab58c55ad5d940676
guha112 <koohajun112@gmail.com>
2025-06-17 15:55:15 +0900

    Update music_links.py

--- music_links.py
+++ music_links.py
@@ -13,5 +13,81 @@ music_links = {
         "https://www.youtube.com/watch?v=RgKAFK5djSk",
         "https://www.youtube.com/watch?v=UceaB4D0jpo"
     ],
-    # 나머지 장르도 동일하게 넣어주세요
+    "클래식": [
+        "https://www.youtube.com/watch?v=_4IRMYuE1hI",
+        "https://www.youtube.com/watch?v=GRxofEmo3HA",
+        "https://www.youtube.com/watch?v=E2LM3ZlcDnk",
+        "https://www.youtube.com/watch?v=Rb0UmrCXxVA",
+        "https://www.youtube.com/watch?v=8Af372EQLck"
+    ],
+    "정돈된 테마 음악": [
+        "https://www.youtube.com/watch?v=bdBuDg7mrT8",
+        "https://www.youtube.com/watch?v=AlTn4nZ7O2M",
+        "https://www.youtube.com/watch?v=92XVwY54h5k",
+        "https://www.youtube.com/watch?v=Oe8Oi5Qn4ZI",
+        "https://www.youtube.com/watch?v=J---aiyznGQ"
+    ],
+    "다양성 있는 셔플 믹스": [
+        "https://www.youtube.com/watch?v=kXYiU_JCYtU",
+        "https://www.youtube.com/watch?v=fLexgOxsZu0",
+        "https://www.youtube.com/watch?v=uelHwf8o7_U",
+        "https://www.youtube.com/watch?v=CevxZvSJLk8",
+        "https://www.youtube.com/watch?v=hLQl3WQQoQ0"
+    ],
+    "몽환적 사운드": [
+        "https://www.youtube.com/watch?v=QD4qoo0U6y8",
+        "https://www.youtube.com/watch?v=DUWLm3K6T2I",
+        "https://www.youtube.com/watch?v=w4cTYnOPdNk",
+        "https://www.youtube.com/watch?v=8UVNT4wvIGY",
+        "https://www.youtube.com/watch?v=kXYiU_JCYtU"
+    ],
+    "감성 발라드": [
+        "https://www.youtube.com/watch?v=YykjpeuMNEk",
+        "https://www.youtube.com/watch?v=450p7goxZqg",
+        "https://www.youtube.com/watch?v=0J2QdDbelmY",
+        "https://www.youtube.com/watch?v=uelHwf8o7_U",
+        "https://www.youtube.com/watch?v=hLQl3WQQoQ0"
+    ],
+    "OST": [
+        "https://www.youtube.com/watch?v=8hP9D6kZseM",
+        "https://www.youtube.com/watch?v=5PSNL1qE6VY",
+        "https://www.youtube.com/watch?v=2e-eXJ6HgkQ",
+        "https://www.youtube.com/watch?v=GC2qk2X3fKA",
+        "https://www.youtube.com/watch?v=HeMeBhkYGlg"
+    ],
+    "신나는 팝": [
+        "https://www.youtube.com/watch?v=fRh_vgS2dFE",
+        "https://www.youtube.com/watch?v=OPf0YbXqDm0",
+        "https://www.youtube.com/watch?v=UprcpdwuwCg",
+        "https://www.youtube.com/watch?v=09R8_2nJtjg",
+        "https://www.youtube.com/watch?v=hT_nvWreIhg"
+    ],
+    "재즈": [
+        "https://www.youtube.com/watch?v=vmDDOFXSgAs",
+        "https://www.youtube.com/watch?v=HMnrl0tmd3k",
+        "https://www.youtube.com/watch?v=Dx5qFachd3A",
+        "https://www.youtube.com/watch?v=vmvgxW2IYI8",
+        "https://www.youtube.com/watch?v=6Whgn_iE5uc"
+    ],
+    "가사 중심 감성곡": [
+        "https://www.youtube.com/watch?v=450p7goxZqg",
+        "https://www.youtube.com/watch?v=nOCbFzKfXZ0",
+        "https://www.youtube.com/watch?v=eVTXPUF4Oz4",
+        "https://www.youtube.com/watch?v=8LhCd1W2V0Q",
+        "https://www.youtube.com/watch?v=JGwWNGJdvx8"
+    ],
+    "레트로팝": [
+        "https://www.youtube.com/watch?v=Zi_XLOBDo_Y",
+        "https://www.youtube.com/watch?v=fJ9rUzIMcZQ",
+        "https://www.youtube.com/watch?v=3JWTaaS7LdU",
+        "https://www.youtube.com/watch?v=0FzI0ibP0Qk",
+        "https://www.youtube.com/watch?v=ZyhrYis509A"
+    ],
+    "드림팝": [
+        "https://www.youtube.com/watch?v=GZ-kZMuTo5I",
+        "https://www.youtube.com/watch?v=E_7fLfxpI4w",
+        "https://www.youtube.com/watch?v=4fndeDfaWCg",
+        "https://www.youtube.com/watch?v=8UVNT4wvIGY",
+        "https://www.youtube.com/watch?v=QD4qoo0U6y8"
+    ]
 }
