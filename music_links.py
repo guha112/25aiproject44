6d5daebfc1169b762418b3c6e66c37ed92efedea
guha112 <koohajun112@gmail.com>
2025-06-17 15:18:14 +0900

    Create music_links.py

---
+++ music_links.py
@@ -0,0 +1,17 @@
+music_links = {
+    "Lofi": [
+        "https://www.youtube.com/watch?v=5qap5aO4i9A",
+        "https://www.youtube.com/watch?v=DWcJFNfaw9c",
+        "https://www.youtube.com/watch?v=hHW1oY26kxQ",
+        "https://www.youtube.com/watch?v=ioNng23DkIM",
+        "https://www.youtube.com/watch?v=7NOSDKb0HlU"
+    ],
+    "EDM": [
+        "https://www.youtube.com/watch?v=60ItHLz5WEA",
+        "https://www.youtube.com/watch?v=Zz6zHmZxF9U",
+        "https://www.youtube.com/watch?v=pXRviuL6vMY",
+        "https://www.youtube.com/watch?v=RgKAFK5djSk",
+        "https://www.youtube.com/watch?v=UceaB4D0jpo"
+    ],
+    # 나머지 장르도 동일하게 넣어주세요
+}
